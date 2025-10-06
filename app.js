e3bb89d24e873096c963e01885c429f187418585
zachshoaib <zacharia.shoaib@aa.com>
2025-10-05 21:10:01 -0500

    adding index.html and favicon

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname));
+
+app.use("/favicon.ico", express.static("public/favicon.svg"));
+
+
 app.get("/", (req, res) => {
-    res.send('Welcome to kombio');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
