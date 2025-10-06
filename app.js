7ef863b3f004bbcba326b754a212c0f206512a9e
zachshoaib <zacharia.shoaib@aa.com>
2025-10-05 22:12:36 -0500

    accepting connections

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { fileURLToPath } from "url";
+import path from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
@@ -9,16 +9,30 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Allow your React app's origin
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(express.static(__dirname));
 
 app.use("/favicon.ico", express.static("public/favicon.svg"));
 
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
