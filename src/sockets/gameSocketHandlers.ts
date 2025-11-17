3449f3483f8dde7e690b9c51258bb0be5610e416
zachshoaib <zachshoaib@pm.me>
2025-11-16 18:18:33 -0600

    feat: begin game and next turn handlers

--- src/sockets/gameSocketHandlers.ts
+++ src/sockets/gameSocketHandlers.ts
@@ -1,9 +1,63 @@
-import { Socket, Server } from "socket.io";
+import { Server } from "socket.io";
+import { activeRooms } from "../models/room.js";
 
-export const beginGameHandler = async (io: Server, socket: Socket, roomId: string) => {
+export const beginGameHandler = async (io: Server, roomId: string) => {
+  const room = activeRooms.get(roomId);
+  if (room) {
+    const turnIndex = room.game.turnIndex;
+
+    const firstPlayer = room.players[turnIndex];
+    if (firstPlayer) {
+      firstPlayer.isTurn = true;
+      io.to(roomId).emit("beginningGame");
+      io.to(roomId).emit("playersList", room.players);
+      const firstPlayerSocket = (await io.in(roomId).fetchSockets()).find(
+        (socket) => socket.data.playerName === firstPlayer.name
+      );
+      if (firstPlayerSocket) {
+        firstPlayerSocket.emit("yourTurn");
+      }
+    }
+  }
+
   console.log("beginGameHandler");
 };
 
-export const endGameHandler = async (io: Server, socket: Socket, roomId: string) => {
+export const nextTurnHandler = async (io: Server, roomId: string) => {
+  const room = activeRooms.get(roomId);
+
+  if (room) {
+    if (room.game.turnIndex == room.players.length - 1) {
+      room.game.turnIndex = 0;
+    } else {
+      room.game.turnIndex += 1;
+    }
+
+    const turnIndex = room.game.turnIndex;
+    const playerTurn = room.players[turnIndex];
+
+    const lastPlayer = room.players.find((player) => player.isTurn == true);
+    if (lastPlayer) {
+      lastPlayer.isTurn = false;
+    }
+
+    if (playerTurn) {
+      playerTurn.isTurn = true;
+      const playerSocket = (await io.in(roomId).fetchSockets()).find(
+        (socket) => socket.data.playerName === playerTurn.name
+      );
+      if (playerSocket) {
+        playerSocket.emit("yourTurn");
+      }
+      io.to(roomId).emit("playersList", room.players);
+    }
+  }
+
+  console.log("nextTurnHandler");
+};
+
+export const endGameHandler = async (io: Server, roomId: string) => {
+  io.to(roomId).emit("endingGame");
+
   console.log("beginGameHandler");
 };
--- src/sockets/gameSocketManager.ts
+++ src/sockets/gameSocketManager.ts
@@ -1,15 +1,19 @@
-import { beginGameHandler, endGameHandler } from "./gameSocketHandlers.js";
+import { beginGameHandler, nextTurnHandler, endGameHandler } from "./gameSocketHandlers.js";
 
 import { Server } from "socket.io";
 
 export function setupGameSocketHandlers(io: Server) {
   io.on("connection", (socket) => {
     socket.on("beginGame", async (roomId) => {
-      await beginGameHandler(io, socket, roomId);
+      await beginGameHandler(io, roomId);
+    });
+
+    socket.on("nextTurn", async (roomId) => {
+      await nextTurnHandler(io, roomId);
     });
 
     socket.on("endGame", async (roomId) => {
-      await endGameHandler(io, socket, roomId);
+      await endGameHandler(io, roomId);
     });
   });
 }
