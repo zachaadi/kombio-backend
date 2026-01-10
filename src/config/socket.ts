9a4f44dba557b72fc584bb492dba1c841dd4793f
zachshoaib <zachshoaib@pm.me>
2026-01-09 22:54:19 -0600

    feat: dont accept requests from localhost

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,7 +1,7 @@
 export const config = {
   port: 3000,
   cors: {
-    origin: ["http://localhost:5173", "https://kombio-frontend.onrender.com"],
+    origin: ["https://kombio-frontend.onrender.com"],
     methods: ["GET", "POST"],
   },
 };
