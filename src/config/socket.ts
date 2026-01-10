b82c4f2db118c1ae085a45abed67291400f90941
zachshoaib <zachshoaib@pm.me>
2026-01-09 22:36:19 -0600

    chore: add process.env.PORT

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,5 +1,5 @@
 export const config = {
-  port: 3000,
+  port: process.env.PORT || 3000,
   cors: {
     origin: ["http://localhost:5173", "https://kombio-frontend.onrender.com"],
     methods: ["GET", "POST"],
