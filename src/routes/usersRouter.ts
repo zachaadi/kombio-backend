5a96267d1db612a376beec655918444432f9ff18
zachshoaib <zachshoaib@pm.me>
2026-01-29 20:50:48 -0600

    feat: create jwt and send it back to frontend

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,16 +1,45 @@
 import express from "express";
 import { postUser } from "../services/usersService.js";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
+const createToken = (username: String) => {
+  const payload = { username: username };
+  const secret = process.env.JWT_SECRET;
+  const options: SignOptions = { expiresIn: "1h" };
+  if (!secret) {
+    throw new Error("JWT is undefined");
+  }
+  return jwt.sign(payload, secret, options);
+};
+
+router.post("/create", async (req, res, next) => {
   try {
     const query = { email: req.body.email, username: req.body.username, password: req.body.password };
-    let result = await postUser(query);
-    res.status(200).json(result);
+    await postUser(query);
+    const token = createToken(req.body.username);
+
+    res.status(201).json(token);
   } catch (error) {
     next(error);
   }
+});
+
+router.post("/login", async (req, res, next) => {
+  //login user
+});
+
+router.get("/:username", async (req, res, next) => {
+  //get profile
+});
+
+router.put("/:username", async (req, res, next) => {
+  //edit profile
+});
+
+router.delete("/:username", async (req, res, next) => {
+  //delete profile
 });
 
 export default router;
